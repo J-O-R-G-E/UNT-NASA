5a63a33587aafa389eaff02eea71dd948687c42b
Jorge Cardona <ce.jorgecardona@gmail.com>
2018-04-08 03:18:33 +0000

    Masks 24 instead of 16

--- netSetup.sh
+++ netSetup.sh
@@ -112,7 +112,7 @@ echo -e "${GREEN}OK${RESET} "
 
 sudo sed -i -e 's/#interface eth0/interface eth0/g' /etc/dhcpcd.conf
 sudo sed -i -e 's/#interface eth0/interface eth0/g' /etc/dhcpcd.conf
-sudo sed -i -e 's/#static ip_address=192.168.0.10\/24/static ip_address=192.168.1.12\/16/g' /etc/dhcpcd.conf 
+sudo sed -i -e 's/#static ip_address=192.168.0.10\/24/static ip_address=192.168.1.12\/24/g' /etc/dhcpcd.conf 
 sudo sed -i -e 's/#static ip6_address=fd51:42f8:caae:d92e::ff\/64/static ip6_address=fd51:42f8:caae:d92e::ff\/64/g' /etc/dhcpcd.conf
 sudo sed -i -e 's/#static routers=192.168.0.1/static routers=192.168.0.1/g' /etc/dhcpcd.conf
 sudo sed -i -e 's/#static domain_name_servers=192.168.0.1 8.8.8.8 fd51:42f8:caae:d92e::1/static domain_name_servers=192.168.0.1 8.8.8.8 fd51:42f8:caae:d92e::1/g' /etc/dhcpcd.conf
